6039f288198e6a8faba05cbceb64d50fc7bef934
Pavel Samokha <pavel@samokha.com>
2016-11-18 16:19:45 +0300

    change key_interrupt asm and added bin

--- key_interrupt.asm
+++ key_interrupt.asm
@@ -3,26 +3,40 @@ kbdb=177562 ; data register for console input
 fonts=177000
 lettersize=10 ; lettersize = 8bytes
 videoram=100000
-start: mov #kdraw,@#kbdaddr ; write our handle to IVT
-kdraw: MOV r0,-(sp) ; push to stack
-   MOV r1,-(sp) ; push to stack
-   MOV r2,-(sp) ; push to stack
-   MOV r3,-(sp) ; push to stack
+;
+.origin 1000
+start: 
+  mov #kdraw,@#kbdaddr ; write our handle to IVT
+  jmp application
+;
+; handle keyboard interrupt
+kdraw:
+;   
+   mov r0,-(sp) ; push to stack
+   mov r1,-(sp) ; push to stack
+   mov r2,-(sp) ; push to stack
+   mov r3,-(sp) ; push to stack
 ;
    movb @#kbdb, r0
    mov #videoram, r1
    mov #lettersize, r2
 ;
-   mul 10, r0
+   mul #10, r0
    add fonts, r0
 ;
    l1: movb (r0)+, r1
-      add 40, r1
+      add #40, r1
       dec r2
       bne l1
 ;
-   MOV (sp)+, r0 ; pop from stack
-   MOV (sp)+, r1 ; pop from stack
-   MOV (sp)+, r2 ; pop from stack
-   MOV (sp)+, r3 ; pop from stack
+   mov (sp)+, r0 ; pop from stack
+   mov (sp)+, r1 ; pop from stack
+   mov (sp)+, r2 ; pop from stack
+   mov (sp)+, r3 ; pop from stack
    rti
+;
+application:
+   l2: nop
+      br l2
+;
+.end start
